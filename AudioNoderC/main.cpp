d71e5c634ab662dc59848372c4a6cb4739ff84af
PostreslesCODE <darakovmaxim@gmail.com>
2026-06-16 05:28:52 +0200

    Made so that the app doesnt allow to be launched multiple times to prevent random behaviour

--- AudioNoderC/main.cpp
+++ AudioNoderC/main.cpp
@@ -10,6 +10,9 @@
 #include <QVBoxLayout>
 #include <QSystemTrayIcon>
 #include <QMenu>
+#include <QLockFile>
+#include <QDir>
+#include <QMessageBox>
 
 extern "C"{
     #include "service.h"
@@ -21,6 +24,14 @@ int main(int argc, char *argv[]){
     QApplication app(argc,argv);
     app.setQuitOnLastWindowClosed(false);
 
+
+    // Makes sure that the app is not launched twice at the same time cuz that would break the nodes
+    QLockFile lockFile(QDir::temp().absoluteFilePath("audionoder.lock"));
+    if(!lockFile.tryLock(100)){
+
+        QMessageBox::warning(nullptr, "Already Running", "Audio Noder is already running in the system tray.");
+        return 1;
+    }
 
     MainWindow window;
 
